43cc1923be3dcc15fea69a189a3c8e1b9f078728
Dave0512 <dschneiderhoffmann@gmail.com>
2020-01-17 07:59:54 +0100

    Type: feat
    Implement methods:
    1) to sort data ascending by year
    2) drop_na
    3) calculate rolling_avg of the avg_year

--- explore_weather_trends.py
+++ explore_weather_trends.py
@@ -14,10 +14,18 @@ path_source = os.path.join(BASE_DIR + "/1_Sources/")
 print(path_source)
 
 df_city_data_hamburg = pd.read_csv(path_source+"city_data_hamburg.csv")
+df_city_data_hamburg.sort_values("year")
+
+df_city_data_hamburg_without_na = df_city_data_hamburg.dropna()
+
+df_city_data_hamburg_without_na['rolling_avg'] = df_city_data_hamburg_without_na['avg_temp'].rolling(window=7, center=False).mean()
+
+
 
 
 def _main():
     print(df_city_data_hamburg)
+    print(df_city_data_hamburg_without_na)
 
 if __name__ == "__main__":
     _main()
