3ce2c806ffc2a62daa5103825ac3e3d0bbbe1ff1
Dave0512 <dschneiderhoffmann@gmail.com>
2020-01-19 20:27:02 +0100

    Type: feat - Add vlookup for merge local and global df

--- explore_weather_trends.py
+++ explore_weather_trends.py
@@ -39,20 +39,20 @@ class roll_avg:
         df = df.sort_values(self.load_column_to_sort)
         return df
 
-    def _drop_na(self):
-        """ drop_na in the col with data for calculation to recieve correct results """
-        df = self._sort_df()
-        df = df.dropna()
-        return df
-
     def _execute_calc(self):
         """ calc rolling avg """
-        df = self._drop_na()
+        df = self._sort_df()
         df[self.created_column] = df[self.loaded_column_for_calc].rolling(window=self.period_window, center=False).mean()
         return df
 
-    def _add_col_source(self):
+    def _drop_na(self):
+        """ drop_na in the col with data for calculation to recieve correct results """
         df = self._execute_calc()
+        df = df.dropna()
+        return df
+
+    def _add_col_source(self):
+        df = self._drop_na()
         df['file'] = pd.Series()
         df['file'] = df['file'].replace(np.NaN,self.load_source)
         return df
@@ -64,20 +64,21 @@ class roll_avg:
 
 
 
-    def add_visualisation(self):
-        """ add line plot """
-        df = self.final()
-        x = df[self.load_column_to_sort]
-        y = df[self.created_column]
-        label_line_1 = self.load_source # Line 1
-        plt.plot(x,y,label=label_line_1,color='blue',linewidth=2, markersize=12)
-        # plt.plot(x,y,label=label_line_2,color='green', linestyle='dashed',linewidth=2, markersize=12)
-        plt.xlabel(self.load_column_to_sort)
-        plt.ylabel(self.created_column)
-        plt.legend(loc="upper left")
-        plt.show()
+    # def add_visualisation(self):
+    #     """ add line plot """
+    #     df = self.final()
+    #     x = df[self.load_column_to_sort]
+    #     y = df[self.created_column]
+    #     label_line_1 = self.load_source # Line 1
+    #     plt.plot(x,y,label=label_line_1,color='blue',linewidth=2, markersize=12)
+    #     # plt.plot(x,y,label=label_line_2,color='green', linestyle='dashed',linewidth=2, markersize=12)
+    #     plt.xlabel(self.load_column_to_sort)
+    #     plt.ylabel(self.created_column)
+    #     plt.legend(loc="upper left")
+    #     plt.show()
 
-def main():
+# Version 1 - don't work
+def create_concat_df():
     """ def to create objects and execute class methods """
     city_data = roll_avg("city_data_hamburg",".csv","year",'avg_temp','created_col_rol_avg',7)
     global_data = roll_avg("global_data",".csv","year",'avg_temp','created_col_rol_avg',7)
@@ -88,7 +89,49 @@ def main():
     frames = [city_data.final(),global_data.final()]
     df_total = pd.concat(frames,sort=False)
     print(df_total)
+    return df_total
 
+# Version 2 - works
+def create_vlookup():
+    """ def to join global and local roll_avg for visualisation """
+
+    city_data = roll_avg("city_data_hamburg",".csv","year",'avg_temp','created_col_rol_avg',7)
+    global_data = roll_avg("global_data",".csv","year",'avg_temp','created_col_rol_avg',7)
+     # col for vlookup
+    df_local = pd.DataFrame(city_data.final())
+    df_global = pd.DataFrame(global_data.final())
+
+    df_merged = df_local.merge(df_global, on = "year", how='left')
+    df_merged.rename(columns={'created_col_rol_avg_x':'roll_avg_local','created_col_rol_avg_y':'roll_avg_global'},inplace=True)
+    df_merged = df_merged[['year','roll_avg_local','roll_avg_global']]
+    print(df_merged.info())
+    # return df_merged
+# df_Artikel_inkl_Klassifikation_Part_2.rename(columns={['Lieferant_ArtikelNr_Global_Substitut':'Lieferant_ArtikelNr_Global','Lieferant_ID_Substitut':'Lieferant_ID','Lieferant_ArtikelNr_Substitut':'Lieferant_ArtikelNr','Lieferant_Artikelbeschreibung_Substitut':'Lieferant_Artikelbeschreibung'},inplace=True)
+
+
+
+def add_visualisation():
+    """ add line plot """
+    df = create_vlookup()
+    df_1 = df["File"]=="city_data_hamburg"
+    x_1 = df_1["year"]
+    y_1 = df_1["created_col_rol_avg"]
+
+    df_2 = df["File"]=="global_data"
+    x_2 = df_2["year"]
+    y_2 = df_2["created_col_rol_avg"]
+    label_line_1 = "city_data_hamburg" # Line 1
+    label_line_2 = "global_data" # Line 2
+    plt.plot(x_1,y_1,label=label_line_1,color='blue',linewidth=2, markersize=12)
+    plt.plot(x_2,y_2,label=label_line_2,color='green',linewidth=2, markersize=12)
+    plt.xlabel(df["year"])
+    plt.ylabel(df["created_col_rol_avg"])
+    plt.legend(loc="upper left")
+    plt.show()
+
+def main():
+    create_vlookup()
+    # add_visualisation()
     # city_data.add_visualisation()
     # global_data.add_visualisation()
 
